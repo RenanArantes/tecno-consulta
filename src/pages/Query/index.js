a43fcb473fa5d1f8c13a9b5b95b897cf9f2d9cf7
RenanArantes <renan.arantes@rede.ulbra.br>
2020-05-19 00:18:23 -0300

    query structure

--- src/pages/Query/index.js
+++ src/pages/Query/index.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
+import formatValue from '../../utils/formatValue'
+
 import Header from '../../components/Header';
 import Container from '../../components/Container';
 
-import { Table, TableHeader, Title } from './styles';
-import { queries } from '@testing-library/react';
+import { Title, Value, Field } from './styles';
 
 export default function Query(props) {
   const [data, setData] = useState()
@@ -18,54 +19,69 @@ export default function Query(props) {
   return(
     <>
       <Header />
-      <Container>
-        {data ?
-        <ul>
-          {data.atividade_principal.map(element => <li>Atividade Principal: {element.code} - {element.text}</li>)}
-          <li>Data da situação: {data.data_situacao}</li>
-          <li>Complemento: {data.complemento}</li>
-          <li>Nome da Empresa: {data.nome}</li>
-          <li>UF: {data.uf}</li>
-          <li>Telefone: {data.telefone}</li>
-          <li>Email: {data.email}</li>
-          {data.atividades_secundarias.map(element => <li>Atividades Secundarias: {element.code} - {element.text}</li>)}
-          {data.qsa.map(element => {
-            return (
-            <>
-              <li>{element.nome} - {element.qual}</li>
-              {element.pais_origem ? <li>{element.pais_origem}</li> : null}
-              {element.qual_rep_legal ?
-                <li>{element.qual_rep_legal} - {element.nome_rep_legal}</li> : null }
-            </>
-            )
-          })}
-          <li>Situacao: {data.situacao}</li>
-          <li>Bairro: {data.bairro}</li>
-          <li>Logradouro: {data.logradouro}</li>
-          <li>Numero: {data.numero}</li>
-          <li>CEP: {data.cep}</li>
-          <li>Municipio: {data.municipio}</li>
-          <li>Porte: {data.porte}</li>
-          <li>Data de abertura: {data.abertura}</li>
-          <li>{data.natureza_juridica}</li>
-          <li>{data.fantasia}</li>
-          <li>{data.cnpj}</li>
-          <li>{data.ultima_atualizacao}</li>
-          <li>{data.status}</li>
-          <li>{data.tipo}</li>
-          {data.efr ? <li>{data.efr}</li> : null}
-          {data.motivo_situacao ? <li>{data.motivo_situacao}</li> : null}
-          {data.situacao_especial ?
-            <>
-              <li>{data.situacao_especial}</li>
-              <li>{data.data_situacao_especial}</li>
-            </> : null
-          }
-          <li>{data.capital_social}</li>
-        </ul>
-         :
-         <Title>Nome da Empresa</Title>}
-      </Container>
+      {data ?
+        <>
+          <Title>{data.fantasia}</Title>
+          <Container>
+            <h1>Empresa</h1>
+            <hr/>
+            <label>
+              <Field>
+                CNPJ: <Value>{data.cnpj}</Value> Data de abertura: <Value>{data.abertura}</Value>
+              </Field>
+            </label>
+
+            <Field>
+              Nome empresarial: <Value>{data.nome}</Value> Tipo: <Value>{data.tipo}</Value>
+            </Field>
+          </Container>
+          <Container>
+            <ul>
+            {data.atividade_principal.map(element => <li>Atividade Principal: {element.code} - {element.text}</li>)}
+            <li>Complemento: {data.complemento}</li>
+            <li>UF: {data.uf}</li>
+            <li>Telefone: {data.telefone}</li>
+            <li>Email: {data.email}</li>
+            {data.atividades_secundarias.map(element => <li>Atividades Secundarias: {element.code} - {element.text}</li>)}
+            {data.qsa.map(element => {
+              return (
+                <>
+                <li>{element.nome} - {element.qual}</li>
+                {element.pais_origem ? <li>{element.pais_origem}</li> : null}
+                {element.qual_rep_legal ?
+                  <li>{element.qual_rep_legal} - {element.nome_rep_legal}</li> : null }
+              </>
+              )
+            })}
+            <li>Data da situação: {data.data_situacao}</li>
+            <li>Situacao: {data.situacao}</li>
+            <li>Bairro: {data.bairro}</li>
+            <li>Logradouro: {data.logradouro}</li>
+            <li>Numero: {data.numero}</li>
+            <li>CEP: {data.cep}</li>
+            <li>Municipio: {data.municipio}</li>
+            <li>Porte: {data.porte}</li>
+            <li>Natureza juridica: {data.natureza_juridica}</li>
+            <li>Ultima atualizacao: {data.ultima_atualizacao}</li>
+            <li>Status: {data.status}</li>
+            <li>Tipo: {data.tipo}</li>
+            {data.efr ? <li>EFR: {data.efr}</li> : null}
+            {data.motivo_situacao ? <li>Motivo{data.motivo_situacao}</li> : null}
+            {data.situacao_especial ?
+              <>
+                <li>Situacao especial: {data.situacao_especial}</li>
+                <li>Data da situacao especial: {data.data_situacao_especial}</li>
+              </> : null
+            }
+            <li>Capital Social: {formatValue(data.capital_social)}</li>
+          </ul>
+          </Container>
+        </>
+      :
+        <Container>
+          <Title>Vazio</Title>
+        </Container>
+      }
     </>
   )
 }
--- src/pages/Query/styles.js
+++ src/pages/Query/styles.js
@@ -1,29 +1,8 @@
 import styled from 'styled-components';
 
-export const Table = styled.table`
-  box-sizing: border-box;
-  padding: 5px;
-  display: table;
-  margin: 0 auto;
-  min-width: 320px;
-  margin-top: 40px;
-
-  position: relative;
-
-  border: 1px;
-  border-spacing: 0;
-  width: 17cm;
-  line-height: 9pt;
-`;
-
-export const TableHeader = styled.tbody`
-  img {
-    width: 60px;
-    height: 60px;
-  }
-`;
-
 export const Title = styled.h1`
-
 `
 
+export const Field = styled.text``
+
+export const Value = styled.strong``
