443bd3151017b0a344651252fa34971af006fe2c
Renan <malevo18@gmail.com>
2020-05-22 14:28:50 -0300

    Updates

---
+++ precache-manifest.3631d4f9baa4b80d159ed9833bf5a75f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f752e3cb135313ad7086690795adb9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1561fd36facf83aa8930",
+    "url": "/static/js/2.1971112a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1971112a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "126ebd74a10060765f28",
+    "url": "/static/js/main.a49380a1.chunk.js"
+  },
+  {
+    "revision": "0ea619bd42491397897b",
+    "url": "/static/js/runtime-main.11e65a7e.js"
+  },
+  {
+    "revision": "68993b76131afeb3097f16ce75f7814e",
+    "url": "/static/media/logo.68993b76.png"
+  }
+]);
