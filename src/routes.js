f79b44f4e15f25a448b65c79aedd69e85e407cd7
Renan <malevo18@gmail.com>
2020-05-22 16:28:40 -0300

    BrowserRouter basename

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { HashRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main';
 import Query from './pages/Query';
 
 export default function Routes() {
   return(
-    <HashRouter basename="/">
+    <BrowserRouter basename="tecno-consulta">
       <Switch>
         <Route path="/" exact component={Main}/>
         <Route path="/query" component={Query}/>
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
