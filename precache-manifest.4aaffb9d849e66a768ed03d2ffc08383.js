2245f25123911b88c8c58662ef987a61be2f0c9e
Renan <malevo18@gmail.com>
2020-05-22 14:35:21 -0300

    Updates

---
+++ precache-manifest.4aaffb9d849e66a768ed03d2ffc08383.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792460c41074bfc9b330d47870a38833",
+    "url": "/tecno-consulta/index.html"
+  },
+  {
+    "revision": "f43dc74bbe71c916f9fe",
+    "url": "/tecno-consulta/static/js/2.c666ef76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tecno-consulta/static/js/2.c666ef76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac981237e9519805ee1",
+    "url": "/tecno-consulta/static/js/main.24a1caf4.chunk.js"
+  },
+  {
+    "revision": "25d7bad8d0472c849ee3",
+    "url": "/tecno-consulta/static/js/runtime-main.fadb155b.js"
+  },
+  {
+    "revision": "68993b76131afeb3097f16ce75f7814e",
+    "url": "/tecno-consulta/static/media/logo.68993b76.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3631d4f9baa4b80d159ed9833bf5a75f.js"
+  "/tecno-consulta/precache-manifest.4aaffb9d849e66a768ed03d2ffc08383.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tecno-consulta/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
